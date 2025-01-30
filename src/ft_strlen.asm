de562d1a563f64b9128df79540fe5847040d003f
Vincent Ockers <vincentbockers@gmail.com>
2025-01-30 11:34:28 +0100

    Add docs to ft_strlen

--- src/ft_strlen.asm
+++ src/ft_strlen.asm
@@ -3,16 +3,16 @@ global ft_strlen
 section .text
 
 ft_strlen:
-    xor rax, rax
-    test rdi, rdi    
-    jz .exit
+    xor rax, rax  ; initialize rax to 0
+    test rdi, rdi ; check if rdi (*str) is null
+    jz .exit      ; if it is, return 0
 
 .loop:
-    cmp byte [rdi], 0
-    jz .exit 
-    inc rdi
-    inc rax
-    jmp .loop
+    cmp byte [rdi], 0 ; check if the current byte is null (end of string)
+    jz .exit          ; if it is, return rax (length of string)
+    inc rdi           ; move to the next byte
+    inc rax           ; increment the length of the string
+    jmp .loop         ; repeat the loop
 
 .exit:
     ret
